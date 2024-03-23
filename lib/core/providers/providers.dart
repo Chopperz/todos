c42db222fd6c7967fa465b396fa78b42c7cd2e66
TskDevs <58081289+Chopperz@users.noreply.github.com>
2024-03-24 03:01:10 +0700

    Create Splash screen, Main app and validate authentication

---
+++ lib/core/providers/providers.dart
@@ -0,0 +1,3 @@
+export 'favorites_bloc/favorites_bloc.dart';
+export 'settings_cubit/settings_cubit.dart';
+export 'user_bloc/user_bloc.dart';
---
+++ lib/features/home/screen/home_layout.dart
@@ -0,0 +1,14 @@
+part of 'home_screen.dart';
+
+class HomeLayout extends StatelessWidget {
+  const HomeLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+
+      ],
+    );
+  }
+}

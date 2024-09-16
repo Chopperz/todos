fee796969bbdc00c480b06469787e8d4f50a6cc4
Chopper <58081289+Chopperz@users.noreply.github.com>
2024-09-16 23:43:15 +0700

    Dart fixes as an import as an annotations.

--- lib/core/extensions/src/context.dart
+++ lib/core/extensions/src/context.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:todos_by_bloc/core/constants/app_constants.dart';
 
 extension BuildContextExtensions<T> on BuildContext {
   MediaQueryData get mediaQuery => MediaQuery.of(this);
